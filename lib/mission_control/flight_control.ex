d1937936d50cef4d395f04cc5a33e7693e7f6d62
James Hicks <jhicks@doximity.com>
2020-05-12 13:37:59 -0600

    Initial pass at flight control

---
+++ lib/mission_control/flight_control.ex
@@ -0,0 +1,45 @@
+defmodule MissionControl.FlightControl do
+  use GenServer
+
+  alias MissionControl.{MissionPlan, FlightSegment}
+
+  def start_link(args) do
+    GenServer.start_link(__MODULE__, args)
+  end
+
+  #### Callbacks ####
+
+  @impl true
+  def init(args) do
+    %MissionPlan{} = plan = Keyword.fetch!(args, :plan)
+
+    state = %{
+      plan: plan,
+      distance_remaining: plan.distance
+    }
+
+    enqueue_tick()
+
+    {:ok, state}
+  end
+
+  def handle_info(:tick, %{distance_remaining: distance_remaining} = state)
+      when distance_remaining == 0 do
+    {:stop, :normal, state}
+  end
+
+  @impl true
+  def handle_info(:tick, %{plan: plan, distance_remaining: distance_remaining} = state) do
+    enqueue_tick()
+
+    new_plan = %{plan | distance: distance_remaining}
+    segment = FlightSegment.calculate(new_plan)
+    state = %{state | distance_remaining: distance_remaining - segment.distance}
+
+    {:noreply, state}
+  end
+
+  defp enqueue_tick() do
+    Process.send_after(self(), :tick, 1_000)
+  end
+end
